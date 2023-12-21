6ff6bc281dd454a28f2ff9dd67cc3f060145e1cd
Aditya <atripathi1618@gmail.com>
2023-12-21 11:45:27 +0530

    Update with blink example and instructions

---
+++ blink/blink.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "ad9959.h"
+#include "hardware/clocks.h"
+#include "hardware/dma.h"
+#include "hardware/flash.h"
+#include "hardware/pio.h"
+#include "hardware/spi.h"
+#include "pico/multicore.h"
+
+int main() {
+
+    const uint led_pin = 25;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    // Loop forever
+    while (true) {
+
+        // Blink LED
+        printf("Blinking!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
+}
